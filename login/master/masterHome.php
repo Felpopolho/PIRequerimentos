f1cc9ff9a8cd8906a0701decc388d3e2df3c5ff9
Felipe Tigre <202013600012@ifba.edu.br>
2023-08-11 13:05:08 -0300

    Add: Funções de sair e home para cada tipo de usuário

---
+++ login/master/masterHome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página Inicial</title>
+    </head>
+
+    <body>
+        <a href="sair.php">Sair</a>
+    </body>
+</html>
