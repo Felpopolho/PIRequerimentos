0560db19650540601fa14c74d622f035e611e2fa
Gabriel nunes <202013600057@ifba.edu.br>
2023-08-27 19:14:21 -0300

    add: home css

--- login/home/home.php
+++ login/home/home.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Página Inicial</title>
+        <link rel="stylesheet" href="style.css">
         <meta name="description" id="description" 
         content="Bem-vindo! Sistema integrado de requerimentos estudantis do IFBA Campus Eunápolis">
     </head>
@@ -14,25 +15,37 @@
     <?php
         session_start();
         if (isset($_SESSION['idMaster'])){
-            echo "<div class='titulo'>
-                    <h1>Bem vindo, Admin!</h1>
-                </div>
-
-                <div class='sair'>
-                    <a href='sair.php'>Sair</a>
-                </div>
-
-                <div class='bloco'>
-                    <div class='quantidade'>
-                        <h2>Quantidade de Requerimentos</h2>
+            echo   "<div class='titulo'>
+                        <h1>Bem vindo, Admin!</h1>
+                    </div>
+                    <div class='sair'>
+                        <a href='sair.php' class='botaosuperior'>Sair</a>
                     </div>
 
-                    <div class='subtitulo'>
+                    <div class='container'>
+                        <div class='quantidade'>
+                        <h2>cursos cadastrados</h2>
+                        <a href='relatorio'><button class='meubotao'>Visualizar</button></a>
+                        </div>
+                        <div class='coordenadores'>
                         <h2>Coordenadores cadastrados</h2>
-                    </div>
+                        <a href='relatorio'><button class='meubotao'>Visualizar</button></a>
+                        </div>
+                        <div class='coordenadorees'>
+                        <h2>turmas cadastradas</h2>
+                        <a href='relatorio'><button class='meubotao'>Visualizar</button></a>
+                        </div>
+                    
 
-                    <a href='relatorio'><button>Visualizar</button></a>
-                </div>";
+                    
+                   </div>";
+                        
+                        
+
+                               
+                         
+                        
+                    
         }elseif (isset($_SESSION['SIAPE'])){
             echo "<div class='titulo'>
                     <h1>Bem vindo, Coordenador!</h1>
