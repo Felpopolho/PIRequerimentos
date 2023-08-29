184a51630665c288cfb185b843c39d89efb3d420
Felipe Tigre <202013600012@ifba.edu.br>
2023-08-29 15:03:35 -0300

    Fix: cadastro.php include banco

--- login/home/home.php
+++ login/home/home.php
@@ -15,37 +15,37 @@
     <?php
         session_start();
         if (isset($_SESSION['idMaster'])){
-            echo   "<div class='titulo'>
-                        <h1>Bem vindo, Admin!</h1>
-                    </div>
-                    <div class='sair'>
-                        <a href='sair.php' class='botaosuperior'>Sair</a>
-                    </div>
+            echo "<div class='titulo'>
+                    <h1>Bem vindo, Admin!</h1>
+                </div>
 
-                    <div class='container'>
-                        <div class='quantidade'>
+                <div class='sair'>
+                    <a href='sair.php' class='botaosuperior'>Sair</a>
+                </div>
+
+                <div class='bloco'>
+                    <div class='quantidade'>
                         <h2>cursos cadastrados</h2>
+                    </div>
                         <a href='relatorio'><button class='meubotao'>Visualizar</button></a>
-                        </div>
-                        <div class='coordenadores'>
+                </div>
+
+                <div class='bloco'>
+                    <div class='quantidade'>
                         <h2>Coordenadores cadastrados</h2>
+
                         <a href='relatorio'><button class='meubotao'>Visualizar</button></a>
-                        </div>
-                        <div class='coordenadorees'>
+                    </div>
+                </div>
+
+                <div class='bloco'>
+                    <div class='quantidade'>
                         <h2>turmas cadastradas</h2>
+
                         <a href='relatorio'><button class='meubotao'>Visualizar</button></a>
-                        </div>
-                    
+                    </div>
+                </div>";
 
-                    
-                   </div>";
-                        
-                        
-
-                               
-                         
-                        
-                    
         }elseif (isset($_SESSION['SIAPE'])){
             echo "<div class='titulo'>
                     <h1>Bem vindo, Coordenador!</h1>
