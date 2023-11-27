a24b4224a45f8a10b5b312b2fb65505c2eb2b53d
Alexander <alexguilherme1gc@gmail.com>
2023-11-27 15:43:41 -0300

    Update styleAlunoFunctionsPage.css and fix page title

--- home/aluno/relatorios/relatorioRequerimentos.php
+++ home/aluno/relatorios/relatorioRequerimentos.php
@@ -3,12 +3,12 @@
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>Requerimentos enviados</title>
         <meta name="description" id="description" 
         content="Bem-vindo! Sistema integrado de requerimentos estudantis do IFBA Campus Eunápolis">
-        <link rel="stylesheet" href="../styleAlunoFunctions.css">
+        <link rel="stylesheet" href="../styleAlunoFunctionsPage.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>Requerimentos enviados</title>
     </head>
 
     <body>
@@ -18,20 +18,11 @@
 
                 <img class='logo' src='../../SAR_logo_2.png'>
                     
-                <h2>Requerimentos</h2>
+                <h2>Visualizar Requerimentos</h2>
                         
                 <a href='../../home.php' class='Btn'>
                     <div class='sign'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
-                            <g clip-path="url(#clip0_161_37)">
-                                <path d="M83.3332 45.8333H32.6248L55.9165 22.5416L49.9998 16.6666L16.6665 50L49.9998 83.3333L55.8748 77.4583L32.6248 54.1666H83.3332V45.8333Z" fill="white"/>
-                            </g>
-                            <defs>
-                                <clipPath id="clip0_161_37">
-                                    <rect width="100" height="100" fill="white"/>
-                                </clipPath>
-                            </defs>
-                        </svg>
+                        <span class='material-symbols-outlined'>arrow_back</span>
                     </div>
                 </a>
             </div>
